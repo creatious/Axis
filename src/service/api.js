f0e62c332d5733ba381dca0200434497fe63bb28
creatious <nwabuezestephen27@gmail>
2022-03-12 22:24:49 +0100

    changed server port to heroku port

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export default axios.create({
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    baseURL: 'http://localhost:3001'
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  baseURL: 'http://localhost:4000'
 })
